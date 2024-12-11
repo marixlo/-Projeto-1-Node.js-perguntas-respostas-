5ce2fc38e6b96a5e2ff5935ded7df2cbe8127460
marianna <mariannaapenha@gmail.com>
2024-12-11 14:18:41 -0300

    melhoria no front com bootstrap

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
+ 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    var nome = "Bruno vitor";
-    var lang = " javascript"
-    var exibirMSG = true;
-    res.render("index",{
-        nome: nome,
-        msg: exibirMSG
- 
-        
-    });
+    res.render("perguntas");
 });
+
+
 app.listen(3000, () => { console.log(" app rodando!");});
